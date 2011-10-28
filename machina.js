1486b9f3f6940fe56a256d42b9aee696ef10141b
Brian Cavalier <brian@briancavalier.com>
2011-10-28 17:21:17 -0400

    Initial working version of machine-level events, with fully async event dispatch.  Not sure yet if machine-level or run-level events, or both, are better

--- machina.js
+++ machina.js
@@ -1,9 +1,15 @@
 (function(define) {
 
 define(['./support/when'], function(when) {
-    
-    function noop() {}
-    
+
+    function removeFromArray(array, item) {
+        var i = 0;
+
+        for(;item !== array[i]; i++) {}
+
+        i < array.length && array.splice(i, 0)
+    }
+
     function rejected(reason) {
         var d = when.defer();
         d.reject(reason);
@@ -11,19 +17,18 @@ define(['./support/when'], function(when) {
     }
 
     function createState(name, stateDef, transition) {
-        var state, transitions;
+        var state, transitions, event;
 
         transitions = stateDef.transitions;
-        state = {
-            name: name,
-            isFinal: !transitions || !!stateDef.isFinal
-        };
+        state = new State(name, !transitions || !!stateDef.isFinal);
 
         if(transitions) {
-            for(var event in transitions) {
-                if(transitions.hasOwnProperty(event))
-                state[event] = function() {
-                    return transition(event, transitions[event]);
+            
+            for(event in transitions) {
+                if(transitions.hasOwnProperty(event)) {
+                    state[event] = function() {
+                        return transition(state, event, transitions[event]);
+                    }
                 }
             }
         }
@@ -31,31 +36,125 @@ define(['./support/when'], function(when) {
         return state;
     }
 
+    var Evented = {
+        on: function(actions) {
+            var events, remove;
+
+            events = this._events;
+
+            if(typeof actions === 'function') {
+                events.start.push(actions);
+                remove = function() {
+                    removeFromArray(events, actions);
+                }
+            } else {
+                var start, end;
+
+                start = actions.start;
+                end   = actions.end;
+
+                start && events.start.push(start);
+                end   && events.end.push(end);
+
+                remove = function() {
+                    if(start) removeFromArray(events.start, start);
+                    if(end)   removeFromArray(events.end,   end);
+                }
+            }
+
+            return remove;
+        },
+        
+        _emit: function(event, data) {
+            var actions;
+            
+            actions = this._events[event];
+
+            return when.reduce(actions, function(value, nextAction) {
+                return nextAction(value);
+            }, data);
+        }
+    };
+    
+    function makeEvented(target) {
+        target._events = {
+            start: [],
+            end:   []
+        };
+
+        return target;
+    }
+
+    function State(name, isFinal) {
+        this.name = name;
+        this.isFinal = isFinal;
+        makeEvented(this);
+    }
+    
+    State.prototype = {
+        on: function(actions) {
+            Evented.on.call(this, { start: actions.enter||actions, end: actions.leave });
+        },
+
+        enter: function(data) {
+            return Evented._emit.call(this, 'start', data);
+        },
+        
+        leave: function(data) {
+            return Evented._emit.call(this, 'end', data);
+        }
+    };
+
+    function Run() {
+        makeEvented(this);
+    }
+
+    function applyTransition(state, event) {
+        var transition = state[event];
+
+        return typeof transition === 'function'
+            ? transition()
+            : rejected(event);
+    }
+
     /**
      * Creates a state machine
      * @param stateTable {Object} the state and transition definitions
      */
     function Machina(stateTable) {
-        var blueprint, stateDefs, states, state;
+        var self, blueprint, stateDefs, states, state;
 
+        self = this;
         stateDefs = stateTable.states;
         states = {};
 
-        function transition(event, toName) {
-            var deferred, to;
+        function transition(from, event, toName) {
+            var to = states[toName];
 
-            deferred = when.defer();
-            to = states[toName];
-
-            if(to) {
-                deferred.resolve(to);
-
-            } else {
-                deferred.reject(event);
-
+            function transitionStart(data) {
+                return Evented._emit.call(self, 'start', data);
             }
 
-            return deferred.promise;
+            function leaveFrom(data) {
+                return from.leave(data);
+            }
+
+            function enterTo(data) {
+                return to.enter(data);
+            }
+
+            function transitionEnd(data) {
+                return Evented._emit.call(self, 'end', data).then(function() { return to; });
+            }
+
+            var steps = [transitionStart, leaveFrom, enterTo, transitionEnd];
+            return to
+                ? when.reduce(steps,
+                    function(val, nextStep) {
+                        return nextStep(val);
+                    })
+                : rejected(event);
+
         }
 
         for(state in stateDefs) {
@@ -76,19 +175,10 @@ define(['./support/when'], function(when) {
                 origTransition = this.transition;
                 this.transition = function(event) {
                     return when(promise, function() {
-                        return self.transition(event);
+                        return origTransition(event);
                     });
                 };
 
-                function applyTransition(state, event) {
-                    var transition = state[event];
-                    if (typeof transition === 'function') {
-                        return transition();
-                    } else {
-                        return rejected(event);
-                    }
-                }
-                
                 function completeTransition(val) {
                     self.transition = self.state.isFinal
                         ? rejected
@@ -110,7 +200,7 @@ define(['./support/when'], function(when) {
 
                 return promise;
             },
-            
+
             isFinal: function() {
                 return this.state.isFinal;
             }
@@ -119,10 +209,17 @@ define(['./support/when'], function(when) {
         this.start = function() {
             Run.prototype = blueprint;
             return new Run();
+        };
+
+        makeEvented(this);
+        this.on = function(what, actions) {
+            if(states[what]) {
+                states[what].on(actions);
+            } else {
+                Evented.on.call(this, actions);
+            }
         }
     }
-
-    function Run() {}
 
     Machina.prototype = {
         accepts: function(events) {
